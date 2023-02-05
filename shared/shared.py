4213de6a2c8bf5c50155f2455de205c44a214aa9
TheoExe <tob2zd@virginia.edu>
2023-02-05 01:06:38 -0500

    First editted release

--- shared/shared.py
+++ shared/shared.py
@@ -18,5 +18,11 @@ def clean_string(str_word):
 import re
 def space_compress(stocomp):
     assert isinstance(stocomp, str), "Expected str but got {} instead".format(type(stocomp))
+        
     comp = re.sub(r'\s+', ' ', stocomp)
+                
+    #secondary boolean check for function input object type str
+    if isinstance(stocomp,str) == False:
+        print("Expected str but got {} instead".format(type(stocomp)))
+
     return comp.strip()
